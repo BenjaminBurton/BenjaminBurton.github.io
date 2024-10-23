f1e1f5120ae082ec1a5fe2987965bf27bd08ff8b
Mr. Burton <benjaminlburton@gmail.com>
2024-10-23 12:42:12 -0400

    add theme

--- docs/conf.py
+++ docs/conf.py
@@ -1,3 +1,7 @@
+import os
+import sys
+sys.path.insert(0, os.path.abspath('docsrc'))
+
 # Configuration file for the Sphinx documentation builder.
 #
 # For the full list of built-in configuration values, see the documentation:
@@ -5,7 +9,6 @@
 
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-
 project = 'ServerlessPy'
 copyright = '2024, Benjamin Burton'
 author = 'Benjamin Burton'
@@ -13,16 +16,29 @@ release = '1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
-extensions = []
-
+extensions = [
+    "sphinx_book_theme"
+]
 templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
-
-
 
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
-html_theme = 'alabaster'
+
+html_theme = 'sphinx_book_theme'
+html_theme_options = {
+    'repository_url': 'https://github.com/zaireali649/luit-oct-2024-purple',
+    'use_repository_button': True,
+    'use_edit_page_button': True,
+    'path_to_docs': 'docs/',
+    'launch_buttons': {
+        'colab_url': 'https://colab.research.google.com/',
+        'binderhub_url': 'https://mybinder.org/',
+    },
+    'show_navbar_depth': 2,
+    'show_toc_level': 14,
+    'theme_toggle': 'dark'  # Enable dark mode
+}
+
 html_static_path = ['_static']
