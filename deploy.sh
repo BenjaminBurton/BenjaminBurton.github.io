66be496bc0bb2d14e277e25de722c999fe83d15c
Mr. Burton <benjaminlburton@gmail.com>
2024-10-24 11:06:48 -0400

    Update Sphinx documentation

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
-# Navigate to your Sphinx project directory and build the docs
-cd docs
-make html
 
+make html
 # Copy the built HTML files to the GitHub repository
 cp -r _build/html/*
 
 # Navigate to the GitHub repository and push the changes
-cd ../your-repo
 git add .
 git commit -m "Update Sphinx documentation"
 git push origin main
